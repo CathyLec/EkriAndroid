762b80741dea8c5ff92c722e68654c497db64e64
Catherine Leclercq Ruiz <catherine.leclercq@upb.edu.co>
2017-10-10 23:16:20 -0500

    Navegacion

--- app/src/main/java/com/movilbox/lector/MainActivity.java
+++ app/src/main/java/com/movilbox/lector/MainActivity.java
@@ -1,15 +1,67 @@
 package com.movilbox.lector;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnEditor, btnTextos, btnLugar, btnLugares, btnCronologia,btn ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnEditor=(Button) findViewById(R.id.btnEditor);
+
+
+    }
+
+    public void EditorDeTexto (View view){
+
+        Intent intent = new Intent(MainActivity.this, Editor.class);
+        startActivity(intent);
+
+    }
+
+    public void Cronologia (View view){
+
+        Intent intent = new Intent(MainActivity.this, Cronologia.class);
+        startActivity(intent);
+
+    }
+
+    public void MostrarTextos (View view){
+
+        Intent intent = new Intent(MainActivity.this, Textos.class);
+        startActivity(intent);
+
+    }
+
+    public void MostrarLugares (View view){
+
+        Intent intent = new Intent(MainActivity.this, Lugares.class);
+        startActivity(intent);
+
+    }
+
+    public void MostrarPersonajes (View view){
+
+        Intent intent = new Intent(MainActivity.this, Personajes.class);
+        startActivity(intent);
+
+    }
+
+    public void EditarPersonaje (View view){
+
+        Intent intent = new Intent(MainActivity.this, Perfil.class);
+        startActivity(intent);
+
+    }
+
+    public void EditarLugar (View view){
+
+        Intent intent = new Intent(MainActivity.this, Lugar.class);
+        startActivity(intent);
+
     }
 }
