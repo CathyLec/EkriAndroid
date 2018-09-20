ee03b230786828d7a2bf4e5f7c911956cb2e8b72
Hjuanma <jm1995217@hotmail.com>
2018-09-19 21:40:46 -0500

    AI-3.1.4 <Juan Manuel@DESKTOP-2KIL3E2 Overwrite remote https://github.com/CathyLec/EkriAndroid.git to local

--- app/src/main/java/com/movilbox/lector/Referencias/FirebaseReferences.java
+++
@@ -1,8 +0,0 @@
-package com.movilbox.lector.Referencias;
-
-/**
- * Created by cathe on 6/11/2017.
- */
-
-public class FirebaseReferences {
-}
--- app/src/main/java/com/movilbox/lector/Referencias/Objetos/Suceso.java
+++
@@ -1,8 +0,0 @@
-package com.movilbox.lector.Referencias.Objetos;
-
-/**
- * Created by cathe on 6/11/2017.
- */
-
-public class Suceso {
-}
