c208a13cc9885c409fe7f1035392a4f03c503511
CathyLec <cathyleru97@gmail.com>
2018-10-04 00:40:00 -0500

    sign in sign up

--- app/src/main/java/com/movilbox/lector/Singin.java
+++ app/src/main/java/com/movilbox/lector/Singin.java
@@ -1,30 +1,98 @@
 package com.movilbox.lector;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Singin extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.movilbox.lector.Referencias.Auth.BaseActivity;
+
+public class Singin extends BaseActivity implements View.OnClickListener {
     private TextView txtSingup;
+    private EditText inputMail;
+    private EditText inputPassword;
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singin);
 
         txtSingup=(TextView) findViewById(R.id.link_Singup);
+        inputMail=(EditText)findViewById(R.id.input_email);
+        inputPassword=(EditText) findViewById(R.id.input_password);
+        progressDialog = new ProgressDialog(this);
+        findViewById(R.id.btn_signin).setOnClickListener(this);
+        mAuth= FirebaseAuth.getInstance();
     }
+
+    public void signIn(){
+        final String email=inputMail.toString().trim();
+        String password=inputPassword.toString().trim();
+
+        if (TextUtils.isEmpty(email)) {//(precio.equals(""))
+            Toast.makeText(this, "Se debe ingresar un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+        progressDialog.setMessage("Realizando consulta en linea...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Singin.this, "Bienvenido: " + inputMail.getText(), Toast.LENGTH_LONG).show();
+                            Textos();
+
+
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {//si se presenta una colisión
+                                Toast.makeText(Singin.this, "Ese usuario ya existe ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Singin.this, "No se pudo registrar el usuario ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
     public void SingUp (View view){
 
         Intent intent = new Intent(Singin.this, Singup.class);
         startActivity(intent);
 
     }
-    public void Textos (View view){
+    public void Textos (){
 
         Intent intent = new Intent(Singin.this, Textos.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        signIn();
     }
 }
--- app/src/main/java/com/movilbox/lector/Singup.java
+++ app/src/main/java/com/movilbox/lector/Singup.java
@@ -1,30 +1,117 @@
 package com.movilbox.lector;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Singup extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Singup extends AppCompatActivity implements View.OnClickListener {
     private TextView txtLogin;
+    private EditText txtName, txtEmail, txtPassword, txtPasswordConfirm;
+    private Button btnSignup;
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
 
+        mAuth = FirebaseAuth.getInstance();
+
         txtLogin=(TextView) findViewById(R.id.link_login);
+        txtName=(EditText) findViewById(R.id.txtName);
+        txtEmail=(EditText) findViewById(R.id.txtEmail);
+        txtPassword=(EditText) findViewById(R.id.txtPassword);
+        txtPasswordConfirm=(EditText) findViewById(R.id.txtPasswordConfirm);
+        btnSignup=(Button) findViewById(R.id.btnSignup);
+
+        progressDialog = new ProgressDialog(this);
+
+        btnSignup.setOnClickListener(this);
     }
+
+    private void registrar() {
+        String name= txtName.getText().toString().trim();
+        String email= txtEmail.getText().toString().trim();
+        String password=txtPassword.getText().toString().trim();
+        String confirmation= txtPasswordConfirm.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this,"Se debe ingresar un nombre",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Se debe ingresar una contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(confirmation)){
+            Toast.makeText(this,"Se debe verificar la contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        while(!password.equals(confirmation)){
+            Toast.makeText(this,"Las contraseñas no coinciden",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(Singup.this,"Se ha registrado el usuario con el email: "+ txtEmail.getText(),Toast.LENGTH_LONG).show();
+                            CreateTextos();
+                        }else{
+
+                            Toast.makeText(Singup.this,"No se pudo registrar el usuario ",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
     public void Login (View view){
 
         Intent intent = new Intent(Singup.this, Singin.class);
         startActivity(intent);
 
     }
-    public void CreateTextos (View view){
+    public void CreateTextos (){
 
         Intent intent = new Intent(Singup.this, Textos.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        registrar();
     }
 }
